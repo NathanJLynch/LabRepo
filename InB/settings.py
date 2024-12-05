61e2c0695c1a61549aba98ef2546b42981c32861
joeinsanity <110877404+joeinsanity@users.noreply.github.com>
2024-12-05 14:05:56 -0600

    accounts and configuration

--- InB/settings.py
+++ InB/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'InB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'TA_Scheduler/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
